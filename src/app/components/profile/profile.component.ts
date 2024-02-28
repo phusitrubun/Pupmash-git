c3d290721cda7ef7efe7b16206abe3981444f813
Waritsara Chaiyamong <65011212076@msu.ac.th>
2024-02-29 01:37:36 +0700

    edit

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from "../header/header.component";
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { Header3Component } from '../header3/header3.component';
 
 
 @Component({
@@ -10,7 +10,7 @@ import { RouterLink } from '@angular/router';
     standalone: true,
     templateUrl: './profile.component.html',
     styleUrl: './profile.component.scss',
-    imports: [CommonModule, HeaderComponent, MatIconModule, RouterLink]
+    imports: [CommonModule, Header3Component, MatIconModule, RouterLink]
 })
 export class ProfileComponent {
 
