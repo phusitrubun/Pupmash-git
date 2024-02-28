48f5d66885e24a12a211b41821b556b6b5947043
phusitrubun <phusitrubun692@gmail.com>
2024-02-28 16:05:13 +0700

    phuupdate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { Main2Component } from './components/main2/main2.component';
 import { ProfilemComponent } from './components/profilem/profilem.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
-  { path: '', component: MainComponent },
-  { path: 'main1', component: Main2Component },
+  { path: 'main1', component: MainComponent },
+  { path: '', component: Main2Component },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'profile', component: ProfilemComponent },
+  { path: 'profilem', component: ProfilemComponent },
+  { path: 'profile', component:ProfileComponent },
 ];
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Header3Component } from "../header3/header3.component";
 
 @Component({
-  selector: 'app-profile',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+    selector: 'app-profile',
+    standalone: true,
+    templateUrl: './profile.component.html',
+    styleUrl: './profile.component.scss',
+    imports: [CommonModule, Header3Component]
 })
 export class ProfileComponent {
 
