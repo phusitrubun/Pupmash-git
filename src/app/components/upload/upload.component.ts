ac5637b4dcd149a0cd5daeb735e2799938812b89
phusitrubun <phusitrubun692@gmail.com>
2024-03-01 01:41:29 +0700

    redesign

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -9,5 +9,22 @@ import { Header3Component } from "../all-header/header3/header3.component";
     imports: [Header3Component]
 })
 export class UploadComponent {
+  imageUrl: string = "";
 
+  constructor() { }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.readURL(file);
+    }
+  }
+
+  readURL(file: File) {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.imageUrl = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
 }
