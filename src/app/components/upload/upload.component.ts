ad8bbfc6990a5cc9992afd0051aa516e9abc2188
phusitrubun <phusitrubun692@gmail.com>
2024-03-02 02:10:28 +0700

    upload

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -9,22 +9,36 @@ import { Header3Component } from "../all-header/header3/header3.component";
     imports: [Header3Component]
 })
 export class UploadComponent {
-  imageUrl: string = "";
+  imageUrl1: string = "../../assets/maxresdefault.jpg";
+  imageUrl2: string = "../../assets/maxresdefault.jpg";
+  imageUrl3: string = "../../assets/maxresdefault.jpg";
+  imageUrl4: string = "../../assets/maxresdefault.jpg";
+  imageUrl5: string = "../../assets/maxresdefault.jpg";
+  // Add imageUrl2, imageUrl3, ... for other image upload fields if needed
 
-  constructor() { }
-
-  onFileSelected(event: any) {
+  onFileSelected(event: any, targetElementId: string) {
     const file = event.target.files[0];
     if (file) {
-      this.readURL(file);
+      this.readURL(file, targetElementId);
     }
   }
 
-  readURL(file: File) {
+  readURL(file: File, targetElementId: string) {
     const reader = new FileReader();
     reader.onload = (e: any) => {
-      this.imageUrl = e.target.result;
+      const targetElement = document.getElementById(targetElementId);
+      if (targetElement) {
+        targetElement.style.backgroundImage = `url(${e.target.result})`;
+      }
     };
     reader.readAsDataURL(file);
+  }
+
+  deleteImage(targetElementId: string) {
+    const targetElement = document.getElementById(targetElementId);
+    if (targetElement) {
+      targetElement.style.backgroundImage = ''; // Clear background image
+    }
+    // Optionally, you can reset imageUrl1, imageUrl2, ... here
   }
 }
