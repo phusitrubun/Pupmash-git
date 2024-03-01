dcfa155eba5e9f6ababf12abf3df6e02d0cde53d
Waritsara Chaiyamong <65011212076@msu.ac.th>
2024-03-01 16:29:10 +0700

    #

--- src/app/components/authen/signup/signup.component.ts
+++ src/app/components/authen/signup/signup.component.ts
@@ -25,10 +25,15 @@ export class SignupComponent implements OnInit {
   name: string = '';
   email: string = '';
   password: string = '';
+  user: any;
+  id: number | undefined;
 
   constructor(private authenService: AuthenService, private router : Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // console.log(this.imageUrl);
+    
+  }
 
   onFileSelected(event: any) {
     const file = event.target.files[0];
@@ -42,23 +47,35 @@ export class SignupComponent implements OnInit {
     reader.onload = (e: any) => {
       this.imageUrl = e.target.result;
     };
-    reader.readAsDataURL(file);
+    reader.readAsDataURL(file);  
   }
+
 
   async signUp() {
     const dbemail = await this.authenService.checkUser(this.email);
     if (!dbemail) {
-      const saltRound = 10;
-      const hashedPassword = await bcrypt.hash(this.password, saltRound);
-      const data = {
-        name: this.name,
-        email: this.email,
-        password: hashedPassword,
-        image: this.imageUrl,
-        type: 1,
-      };
-      this.authenService.InsertUser(data);
-      this.router.navigate(['profile']);
+        const saltRound = 10;
+        const hashedPassword = await bcrypt.hash(this.password, saltRound);
+        const data = {
+            name: this.name,
+            email: this.email,
+            password: hashedPassword,
+            image: this.imageUrl,
+            type: 1,
+        };
+
+        // Insert user data into the database
+        await this.authenService.InsertUser(data);
+
+        // Fetch the inserted user data to get the user ID
+        const user = await this.authenService.checkUser(this.email);
+        if (user) {
+            const userId = user.userID;
+            localStorage.setItem('userID', userId.toString());
+            // Navigate to the profile page using the retrieved user ID
+            this.router.navigate(['profile', userId]);
+        }
     }
-  }
+}
+
 }
